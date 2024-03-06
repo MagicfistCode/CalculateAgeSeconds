6d0229f1d354b61c7eafb91b45981d597ec8a134
Dzaky <117564615+MagicfistCode@users.noreply.github.com>
2024-03-06 16:00:32 +0800

    Add project files.

---
+++ CalculateAgeSeconds/Program.cs
@@ -0,0 +1,23 @@
+﻿using AgeCalculator;
+using AgeCalculator.Extensions;
+
+namespace CalculateAgeSeconds
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string userAge;
+
+            Console.WriteLine("Enter your age in this format - MM/DD/YYYY (For example: 08/20/2003)");
+            userAge = Console.ReadLine();
+
+            var dob = DateTime.Parse(userAge);
+            var age = new Age(dob, DateTime.Today, true);
+            Console.WriteLine($"Total Seconds alive: {age.TotalSeconds}");
+
+            Console.ReadLine();
+        }
+    }
+}
